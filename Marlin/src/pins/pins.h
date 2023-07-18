3373eb62e0b40285fd268b7a7d8ddeadf5890134
Bob-the-Kuhn <bob.kuhn@att.net>
2023-07-18 07:36:23 -0500

    partial load

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -802,7 +802,8 @@
   #include "stm32f4/pins_BLACKPILL_CUSTOM.h"        // STM32F4                              env:STM32F401CD_blackpill_stlink
 #elif MB(I3DBEEZ9_V1)
   #include "stm32f4/pins_I3DBEEZ9.h"                // STM32F4                              env:I3DBEEZ9_V1
-
+#elif MB(NUCLEO_G474RE)
+  #include "stm32g4/pins_NUCLEO_G474RE.h"           // STM32G4                              env:NUCLEO_G474RE
 //
 // ARM Cortex-M7
 //
